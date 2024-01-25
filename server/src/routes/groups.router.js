314532b2578d4430553c224b06845ce1bd0d9e0e
Albert Arakelyan <albertarakelyan1998@gmail.com>
2024-01-25 08:17:27 +0400

    td(18): updated todos for APIs

--- server/src/routes/groups.router.js
+++ server/src/routes/groups.router.js
@@ -18,8 +18,9 @@ groupsRouter.get('/', authMiddleware, httpGetGroups);
 groupsRouter.get('/:groupId', authMiddleware, groupAccessMiddleware, httpGetGroup);
 groupsRouter.delete('/:groupId', authMiddleware, groupAccessMiddleware, httpDeleteGroup); // TODO add permission middleware
 groupsRouter.patch('/:groupId', authMiddleware, groupAccessMiddleware, httpUpdateGroup); // TODO add permission middleware
-// TODO create addUserToGroup API with /:groupId/:userId (role with body)
-// TODO create removeUserFromGroup API with /:groupId/:userId (role with body)
+// TODO create inviteUserToGroup API with /:groupId (role and email with body) create a token with users email and role
+// TODO create addUserToGroup API with /:groupId/:invitationToken (role and email from the token, find the user by email)
+// TODO create removeUserFromGroup API with /:groupId/:userId
 // TODO create editUserGroupRole API with /:groupId/:userId (role with body)
 
 module.exports = groupsRouter;

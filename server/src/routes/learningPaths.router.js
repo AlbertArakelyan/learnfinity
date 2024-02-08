a96ed57630390cb7f1db9d17005c0cc45cff62a0
Albert Arakelyan <albertarakelyan1998@gmail.com>
2024-02-08 08:16:13 +0400

    feat(17): connected learning path access middleware

--- server/src/routes/learningPaths.router.js
+++ server/src/routes/learningPaths.router.js
@@ -8,12 +8,14 @@ const {
 } = require('../controllers/learningPaths.controller');
 
 const { authMiddleware } = require('../middlewares/users.middleware');
+const { learningPathAccessMiddleware } = require('../middlewares/learningPaths.middleware');
 
 const learningPathsRouter = express.Router();
 
 learningPathsRouter.post('/', authMiddleware, httpCreateLearningPath);
 learningPathsRouter.get('/public', authMiddleware, httpGetPublicLearningPaths);
+// TODO get shared learning paths
 learningPathsRouter.get('/', authMiddleware, httpGetUserLearningPaths);
-learningPathsRouter.get('/:learningPathId', authMiddleware, httpGetUserLearningPath);
+learningPathsRouter.get('/:learningPathId', authMiddleware, learningPathAccessMiddleware, httpGetUserLearningPath);
 
 module.exports = learningPathsRouter;

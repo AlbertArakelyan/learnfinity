4d0a44e2e7971fd44bacfe947971384226295afd
Albert Arakelyan <albertarakelyan1998@gmail.com>
2024-02-08 09:01:34 +0400

    todo(17): added TODOs for future APIs

--- server/src/routes/learningPaths.router.js
+++ server/src/routes/learningPaths.router.js
@@ -18,5 +18,7 @@ learningPathsRouter.get('/shared', authMiddleware, httpGetSharedLearningPaths);
 learningPathsRouter.get('/public', authMiddleware, httpGetPublicLearningPaths);
 learningPathsRouter.get('/', authMiddleware, httpGetUserLearningPaths);
 learningPathsRouter.get('/:learningPathId', authMiddleware, learningPathAccessMiddleware, httpGetUserLearningPath);
+// TODO add delete API
+// TODO add update API (for only creators for their ones and only for admins and managers in groups)
 
 module.exports = learningPathsRouter;

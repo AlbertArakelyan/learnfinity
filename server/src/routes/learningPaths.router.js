d3cdec7ac70c11e74cd9cdb1ccfdbe8e8ff32765
Albert Arakelyan <albertarakelyan1998@gmail.com>
2024-03-16 16:11:50 +0400

    fix(17): reordered comments

--- server/src/routes/learningPaths.router.js
+++ server/src/routes/learningPaths.router.js
@@ -21,10 +21,10 @@ learningPathsRouter.get('/public', authMiddleware, httpGetPublicLearningPaths);
 learningPathsRouter.get('/', authMiddleware, httpGetUserLearningPaths);
 learningPathsRouter.get('/:learningPathId', authMiddleware, learningPathAccessMiddleware, httpGetUserLearningPath);
 learningPathsRouter.delete('/:learningPathId', authMiddleware, canEditOrDeleteLearningPathMiddleware, httpDeleteUserLearningPath);
+// Can also be used for sharing by changing sharedUserIds (user can be got by emails in frontend and shared without invitation)
 learningPathsRouter.patch('/:learningPathId', authMiddleware, canEditOrDeleteLearningPathMiddleware, httpEditUserLearningPath);
 
 // TODO add update API (for only creators for their ones and only for admins and managers in groups)
-// TODO add API for sharing your private learning path with other users
 
 // TODO add delete API (only for creators(userId===) in groups)
 

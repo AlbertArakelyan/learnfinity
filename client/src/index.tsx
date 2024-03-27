303fad3989f15db0886140657803d540bd078e1d
Albert Arakelyan <albertarakelyan1998@gmail.com>
2024-03-27 08:54:24 +0400

    feat(37): configured eslint and prettier

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import App from './App';
+
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
 import './assets/styles/index.scss';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <App />

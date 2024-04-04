4363983ba6cad06c694297a1bfa0286f20c61c36
Albert Arakelyan <albertarakelyan1998@gmail.com>
2024-04-04 08:44:12 +0400

    todo(): added small in todo in Modal.tsx component

--- client/src/components/shared/Modal/ModalContainer.tsx
+++ client/src/components/shared/Modal/ModalContainer.tsx
@@ -9,6 +9,7 @@ import { IModalContainerProps } from './types';
 const ModalContainer: FC<IModalContainerProps> = ({ title, isOpen, onClose, children }) => {
   useModalContainer(isOpen);
 
+  // TODO move isOpen check into the View component
   return isOpen ? (
     <Modal isOpen={isOpen} title={title} onClose={onClose}>
       {children}
